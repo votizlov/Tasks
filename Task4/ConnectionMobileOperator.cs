7f47b7b6c06a50f619a293729d6f10ce4298081b
Votizlov <SeriousFox@yandex.ru>
2020-10-08 16:07:48 +0300

    Added 4 task

---
+++ Task4/ConnectionMobileOperator.cs
@@ -0,0 +1,30 @@
+﻿namespace Task4
+{
+    public class ConnectionMobileOperator : MobileOperator
+    {
+        private bool isCostForConnection;
+
+        public new float GetQuality()
+        {
+            if (isCostForConnection)
+            {
+                return 0.7f * GetQuality();
+            }
+            else
+            {
+                return 1.5f * GetQuality();
+            }
+        }
+
+        public ConnectionMobileOperator(string name, float coverCange, float costOfMinute, bool isCostForConnection) :
+            base(name, coverCange, costOfMinute)
+        {
+            this.isCostForConnection = isCostForConnection;
+        }
+
+        public new void PrintInfo()
+        {
+            
+        }
+    }
+}
---
+++ Task4/MobileOperator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task4
+{
+    public class MobileOperator
+    {
+        private String name;
+        private float costOfMinute;
+        private float coverCange;
+
+        public MobileOperator(String name,float coverCange,float costOfMinute)
+        {
+            this.name = name;
+            this.coverCange = coverCange;
+            this.costOfMinute = costOfMinute;
+        }
+
+        public float GetQuality()
+        {
+            return 100 * coverCange / costOfMinute;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(name + coverCange + costOfMinute);
+        }
+    }
+}
