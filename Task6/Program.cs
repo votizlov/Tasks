df1b3e49148a33e95b5571ca0cc27af56c946750
votizlov <seriousfox@yandex.ru>
2020-12-03 01:20:53 +0300

    finished 6?

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -8,7 +8,8 @@ namespace Task6
         {
             List<Abitur> abiturs = new List<Abitur>();
             abiturs.Add(new PartTimeStudent("csf","amm",1));
-            
+            PartTimeStudent student = (PartTimeStudent)abiturs[0];
+            student.PrintInfo();
         }
     }
 }
