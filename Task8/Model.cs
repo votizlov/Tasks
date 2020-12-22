ff567e5e03eee0a6c0512d07b6ddf57651a38924
votizlov <seriousfox@yandex.ru>
2020-12-22 23:26:58 +0300

    Added coroutine

--- Task8/Model.cs
+++ Task8/Model.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Threading;
 
 namespace Task8
 {
@@ -10,32 +11,46 @@ namespace Task8
         public Action stepCallback;
         public Action greenLight;
         private List<SimulatingObject> objects;
-        private bool isSimulating = false;
+        /// <summary>
+        /// time in ms
+        /// </summary>
+        private int timeStamp = 50;
+
+        private DispatcherTimer mainTimer;
+        private DispatcherTimer carSpawnerTimer;
+        private DispatcherTimer pedSpawnerTimer;
 
         public List<SimulatingObject> Objects => objects;
 
         public void StartSim()
         {
             objects = new List<SimulatingObject>();
-            objects.Add(new SimulatingObject(0, 100, 1, 1, 1, 1, "Car"));
-            isSimulating = true;
             
-        }
+            mainTimer = new System.Windows.Threading.DispatcherTimer();
+            mainTimer.Tick += new EventHandler(Update);
+            mainTimer.Interval = new TimeSpan(0,0,0,0,timeStamp);
+            mainTimer.Start();
+            
+            carSpawnerTimer = new System.Windows.Threading.DispatcherTimer();
+            carSpawnerTimer.Tick += new EventHandler(Update);
+            carSpawnerTimer.Interval = new TimeSpan(0,0,0,10,timeStamp);
+            carSpawnerTimer.Start();
+            
+            pedSpawnerTimer = new System.Windows.Threading.DispatcherTimer();
+            pedSpawnerTimer.Tick += new EventHandler(Update);
+            pedSpawnerTimer.Interval = new TimeSpan(0,0,0,5,timeStamp);
+            pedSpawnerTimer.Start();
 
-        private async Task AsyncUpdate()
-        {
-            while (isSimulating)
-            {
-                Update();
-            }
         }
 
         public void StopSim()
         {
-            isSimulating = false;
+            mainTimer.Stop();
+            pedSpawnerTimer.Stop();
+            carSpawnerTimer.Stop();
         }
 
-        private void Update()
+        private void Update(object sender, EventArgs e)
         {
             //update position
             foreach (var obj in objects)
@@ -71,12 +86,14 @@ namespace Task8
         {
         }
 
-        private void SpawnCar()
+        private void SpawnCar(object sender, EventArgs e)
         {
+            objects.Add(new SimulatingObject(0, 100, 0, 1, 1, 1, "Car"));
         }
 
-        private void SpawnPedestrian()
+        private void SpawnPedestrian(object sender, EventArgs e)
         {
+            objects.Add(new SimulatingObject(50, 100, 1, 0, 1, 1, "Pedestrian"));
         }
     }
 }
--- Task8/ModelView.xaml.cs
+++ Task8/ModelView.xaml.cs
@@ -20,8 +20,18 @@ namespace Task8
             
             carBitmapImg = new BitmapImage();
             carBitmapImg.BeginInit();
-            carBitmapImg.UriSource = new Uri(Config.ROAD_IMG_PATH, UriKind.Absolute);
+            carBitmapImg.UriSource = new Uri(Config.CAR_IMG_PATH, UriKind.Absolute);
             carBitmapImg.EndInit();
+            
+            pedBitmapImg = new BitmapImage();
+            pedBitmapImg.BeginInit();
+            pedBitmapImg.UriSource = new Uri(Config.PEDESTRIAN_IMG_PATH, UriKind.Absolute);
+            pedBitmapImg.EndInit();
+            
+            emergencyCarBitmapImg = new BitmapImage();
+            emergencyCarBitmapImg.BeginInit();
+            emergencyCarBitmapImg.UriSource = new Uri(Config.EMERGENCY_CAR_IMG_PATH, UriKind.Absolute);
+            emergencyCarBitmapImg.EndInit();
             
             md = new Model();
             md.stepCallback += Redraw;
@@ -30,6 +40,20 @@ namespace Task8
         private void Redraw()
         {
             Canvas.Children.Clear();
+            
+            Image myImage3 = new Image();
+            BitmapImage bi3 = new BitmapImage();
+            bi3.BeginInit();
+            bi3.UriSource = new Uri(Config.ROAD_IMG_PATH, UriKind.Absolute);
+            bi3.EndInit();
+            myImage3.Height = 400;
+            myImage3.Stretch = Stretch.Fill;
+            myImage3.Source = bi3;
+
+            Canvas.Children.Add(myImage3);
+            Canvas.SetTop(myImage3, 0);
+            Canvas.SetLeft(myImage3, 0);
+            
             foreach(var obj in md.Objects)
             {
                 switch (obj.Tag)
@@ -56,18 +80,7 @@ namespace Task8
 
         private void StartBtn_OnClick(object sender, RoutedEventArgs e)
         {
-            Image myImage3 = new Image();
-            BitmapImage bi3 = new BitmapImage();
-            bi3.BeginInit();
-            bi3.UriSource = new Uri(Config.ROAD_IMG_PATH, UriKind.Absolute);
-            bi3.EndInit();
-            myImage3.Height = 400;
-            myImage3.Stretch = Stretch.Fill;
-            myImage3.Source = bi3;
-
-            Canvas.Children.Add(myImage3);
-            Canvas.SetTop(myImage3, 0);
-            Canvas.SetLeft(myImage3, 0);
+           
             md.StartSim();
             logText.Text = "Sim started";
         }
