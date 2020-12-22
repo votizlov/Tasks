9e636966ab85a52aa2bdea862e0cf72ca1a1f9e9
votizlov <seriousfox@yandex.ru>
2020-12-23 01:12:53 +0300

    Added date/time to log

--- Task8/Model.cs
+++ Task8/Model.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Threading;
 
 namespace Task8
@@ -134,7 +132,7 @@ namespace Task8
 
         private void ToggleTrafficLight(object sender, EventArgs e)
         {
-            textCallback?.Invoke("Traffic light changed");
+            textCallback?.Invoke("Traffic light changed " + DateTime.Now);
             toggleGreenLight?.Invoke();
         }
 
